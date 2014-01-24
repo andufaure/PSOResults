03be6dd9cd2851b89023d72f443e3de7f02ea258
onio <onio@ubuntuk.(none)>
2014-01-24 15:23:49 +0100

    JOB SCRIPT

--- cac_results/job_script.sh
+++ cac_results/job_script.sh
@@ -2,7 +2,7 @@
 
 JAR_PATH="../../PSONB/dist/PSONB.jar"
 INIT="../../instances/fazia/init.csv"
-REF="../../instances/fazia/asvn4.csv"
+REF="../../instances/fazia/asvn1.csv"
 
 [ ! -e $JAR_PATH ] && echo "$JAR_PATH does not exist" 1>&2 && exit 1
 [ ! -e $INIT ] && echo "$INIT does not exist" 1>&2 && exit 1
